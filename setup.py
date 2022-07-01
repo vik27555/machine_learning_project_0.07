dade4bda0e9bae444c01e1c8549d835ea6786f54
vik27555 <101319147+vik27555@users.noreply.github.com>
2022-07-01 19:21:05 +0530

    update

--- setup.py
+++ setup.py
@@ -1,33 +1,38 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
-#Decleration variable for setup function
-PROJECT_NAME="housing-prediction"
-VERSION="0.0.1"
-AUTHOR="Vikash Singh"
-DESCRIPTION="this is my first fsds project"
-PACKAGES=["housing"]
-REQUIRMENTS_FILE_NAME="requirements.txt"
+#Declaring variables for setup functions
+PROJECT_NAME="housing-predictor"
+VERSION="0.0.3"
+AUTHOR="vikash singh"
+DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
+
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+HYPHEN_E_DOT = "-e ."
 
 
-def get_requirements_list()->List[str]:
+def get_requirements_list() -> List[str]:
     """
-    Description: this function is going to return list of requirement
-    mention in requirements.txt
-
-    return this function is going to return a list which contain name of 
-    libraries mention in requirements.txt file
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
     """
-    with open(REQUIRMENTS_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
+
 
 
 setup(
 name=PROJECT_NAME,
 version=VERSION,
-author=AUTHOR,
-description=DESCRIPTION,
-packages=PACKAGES,
-install_requires=get_requirements_list()    
-
+author=AUTHOR, 
+description=DESRCIPTION,
+packages=find_packages(), 
+install_requires=get_requirements_list()
 )
